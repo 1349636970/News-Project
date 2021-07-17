5fadc4099a21082d911f0c1de0a90f375108a09b
saru-s <78458548+saru-s@users.noreply.github.com>
2021-07-17 00:09:54 -0700

    Try

---
+++ front/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
+import Home from './components/pages/Home';
+import Business from './components/pages/Business';
+import Health from './components/pages/Health';
+import InternationalRelations from './components/pages/InternationalRelations';
+import CompareNews from './components/pages/CompareNews';
+import Footer from "./components/Footer";
+import SearchResult from './components/SearchResult';
+
+
+
+
+function App() {
+  return (
+    <>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path = '/' exact component={Home} />
+        <Route path='/business' component={Business} />
+        <Route path='/health' component={Health} />
+        <Route path='/internationalrelations' component={InternationalRelations} />
+        <Route path='/comparenews' component={CompareNews} />
+        <Route path='/searchresult' component={SearchResult} />
+      </Switch>
+      <Footer />
+      
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ front/src/components/pages/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../../App.css';
+import LocalDate from '../LocalDate';
+import Cards from '../Cards';
+
+import Banner from '../Banner';
+
+
+
+function Home() {
+  return (
+    <>
+      
+      <LocalDate />
+      <Banner />
+      <Cards />
+      
+      
+    </>
+  );
+}
+
+export default Home;
