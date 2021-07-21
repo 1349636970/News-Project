5855d4dfa4e98f53b13022dacd56765cf3765a93
1349636970 <a1349636970@gmail.com>
2021-07-21 09:23:44 -0700

    Finish Search Function

--- back/src/main/java/US/SummerChallenge/NewsProject/Services/INewsService.java
+++ back/src/main/java/US/SummerChallenge/NewsProject/Services/INewsService.java
@@ -4,12 +4,13 @@ import US.SummerChallenge.NewsProject.model.dto.NewsDTO;
 
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.List;
 
 /**
  * @Author: Zhenyu Lin
  * @Date: 2021/6/29 1:01
  **/
-public interface iNewsService {
-    NewsDTO getNews();
-    NewsDTO getNews(LocalDate date);
+public interface INewsService {
+    List<NewsDTO> getTodayNews();
+    List<List<NewsDTO>> getNewsBetween(String firstCountry, String secondCountry, String searchKeyWord);
 }
