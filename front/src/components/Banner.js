6c309e94d1c3a59bc7627e2850861b7059eea543
saru-s <78458548+saru-s@users.noreply.github.com>
2021-08-10 11:54:07 -0700

    Fetching

---
+++ front/src/components/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Banner.css';
+
+function Banner() {
+    return (
+        <div className='banner-container'>
+            <div className='banner-text'>
+                <p>In Focus: China, Nepal, United States of America</p>
+                <p>China: Xinhua, CCTV News, People's Daily China</p>
+                <p>Nepal: The Himalayan Times, The Kathmandu Post, Republica</p>
+                <p>USA: USA Today, CBS News, The New York Times</p>
+            </div>
+
+         </div>
+    )
+}
+
+export default Banner;
